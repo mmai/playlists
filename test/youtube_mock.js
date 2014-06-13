7e6bef09e49c9043adaaca0d3e94442ff2d3c099
Henri Bourcereau <henri.bourcereau@gmail.com>
2014-06-13 17:13:16 +0200

    less dependencies

--- test/youtube_mock.js
+++ test/youtube_mock.js
@@ -1,5 +1,4 @@
 var Q = require('q');
-var _ = require('lodash');
 var fs = require('fs');
 var sys = require('sys');
 
@@ -32,13 +31,6 @@ Youtube.prototype.showPlaylist = function(youtube_songs){
                     });
             }
         });
-};
-
-Youtube.prototype.html_page = function(songs){
-  html =  '<html> <head> <script language="javascript"> function show_video(id){ document.getElementById("player").setAttribute("src", "http://www.youtube.com/embed/"+id); } </script> </head> <body onload="show_video(\''+songs[0].id+'\');"><div  style="float:right;"><h3>Click on a title to open it</h3><ul id="playlist">';
-  html += _.map(songs, function(song){return '<li onclick="show_video(\''+song.id+'\');">'+song.title+'</li>';}).join('');
-  html += '</ul></div> <iframe id="player" title="YouTube video player" width="425" height="349" src="" frameborder="0" allowfullscreen></iframe> </body></html>';
-  return html;
 };
 
 module.exports = Youtube;
