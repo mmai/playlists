a10390ff54a71c15b91955c43050fe025abb1926
Henri Bourcereau <henri.bourcereau@gmail.com>
2015-09-12 12:03:31 +0200

    remove Grooveshark (API not available anymore)

--- music_services/index.js
+++ music_services/index.js
@@ -2,7 +2,6 @@ module.exports = {
     youtube: require('./youtube'),
     lastfm: require('./lastfm'),
     soundcloud: require('./soundcloud'),
-    grooveshark: require('./grooveshark'),
     deezer: require('./deezer'),
 //    hypem: require('./hypem'),
     spotify: require('./spotify')
